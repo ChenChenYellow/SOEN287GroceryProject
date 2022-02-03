2acb84ac84908e322dfa0bbcd9c012fb59632620
Yuchen Huang <YuchenHuang97@Gmail.com>
2022-02-03 13:41:08 -0500

    implement header

---
+++ Script/header.js
@@ -0,0 +1,24 @@
+class Header extends HTMLElement{
+    constructor(){
+        super();
+    }
+
+    connectedCallback(){
+        this.innerHTML = `
+        <nav class="navbar navbar-expand-sm bg-light navbar-light">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Users</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Inventory</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Orders</a>
+              </li>
+            </ul>
+        </nav>`;
+    }
+
+}
+customElements.define('header-component', Header);
