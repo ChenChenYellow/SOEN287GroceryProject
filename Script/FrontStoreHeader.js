169210401de98668965c889ca2b67712380e092d
chenchenYellow <yuchenhuang97@gmail.com>
2022-02-21 23:12:00 -0500

    c

--- Script/FrontStoreHeader.js
+++ Script/FrontStoreHeader.js
@@ -1,31 +1,36 @@
 class FrontStoreHeader extends HTMLElement {
   constructor() {
-    super()
+    super();
   }
 
   connectedCallback() {
     this.innerHTML = `
-        <nav class="navbar navbar-expand-sm bg-light navbar-light">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#" id="navbardrop" data-toggle="dropdown">Aisles</a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="P2_Aisle_YCH.html?aislename=biscuit_and_chips">Biscuits and Chips</a>
-                    <a class="dropdown-item" href="P2_Aisle_YCH.html?aislename=dry_food">Dry Food</a>
-                    <a class="dropdown-item" href="P2_Aisle_YCH.html?aislename=milk_and_cheese">Milk and Cheese</a>
-                </div>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Shopping Cart</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Your Orders</a>
-              </li>
-            </ul>
-        </nav>`
+        
+      <div class="wrapper">
+      <div id="logo">
+        <a href="index.html">FRESHCO</a>
+      </div>
+      <nav class="navbar navbar-expand-sm navbar-home">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="aisles.html">Aisles</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="shoppingCart.html">Shopping Cart</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="P5.html">Sign In</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="P6.html">Sign Up</a>
+          </li>
+        </ul>
+      </nav>
+      <div id="layover"></div>
+    </div>`;
   }
 }
-customElements.define('header-component', FrontStoreHeader)
+customElements.define("header-component", FrontStoreHeader);
