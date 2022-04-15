01b7c505f4c2f5f98e3314b4ff1d4a8e01e92d39
Yuchen Huang <YuchenHuang97@Gmail.com>
2022-04-14 23:08:53 -0400

    Update ShowAisle.js

--- Script/ShowAisle.js
+++ Script/ShowAisle.js
@@ -1,25 +1,26 @@
 import { xml2json } from "./xml2json.js";
 
 function readAislesXML(aisleName) {
-  return fetch("./Data/Inventory.xml")
-    .then(function (response) {
-      return response.text();
+  return fetch("./Data/Inventory.xml") // request to access xml file
+    .then(function (response) { // the result of access is 'response'
+      return response.text(); // we pass response.text() to the next block
     })
-    .then(function (data) {
-      let parser = new DOMParser();
+    .then(function (data) { // we receive response.text() and rename it 'data'
+      let parser = new DOMParser(); // we parse 'data' to a json object
       let xmlDoc = parser.parseFromString(data, "text/xml");
-      let ret = xml2json(xmlDoc, "    ");
-      return ret["inventory"];
+      let ret = xml2json(xmlDoc, "    "); // the json object is called 'ret'
+      return ret["inventory"]; // we pass the ret["inventory"] to next block
     })
-    .then(function (data) {
-      const ret = data["product"].filter((x) => {
-        return x["aisle"] == aisleName;
+    .then(function (data) { // we receive ret["inventory"] and rename it 'data'
+      const ret = data["product"].filter((x) => { // data["product"] is an array of json, we loop through it
+        return x["aisle"] == aisleName; // and select all item whose "asile" match our aisle name
       });
-      return ret;
+      return ret; // and return those selected items
     });
 }
 
 function getAisleName() {
+  // Get the aisle name from the url
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const aisleName = urlParams.get("aislename");
@@ -30,6 +31,8 @@ window.addEventListener("load", async (event) => {
   const aisleName = getAisleName();
   const products = await readAislesXML(aisleName);
 
+  // 'products' is an array of json, containing all products of our aisle
+  // Now its time fill the data to the page using html
   let aisleNameH1 = document.getElementById("aisleName");
   let words = aisleName.toLowerCase().split("_");
   for (let i = 0; i < words.length; i++) {
@@ -64,7 +67,7 @@ window.addEventListener("load", async (event) => {
     cardBody.classList.add("card-body", "d-flex", "flex-column");
     let a = document.createElement("a");
     cardBody.appendChild(a);
-    a.href = "./product.html?id=" + products[i]["id"];
+    a.href = "./product.html?id=" + products[i]["id"]; // When click a product, it goes to product.html with its product id in the url
     a.classList.add("text-decoration-none", "stretched-link");
     a.style.color = "inherit";
     let hr = document.createElement("hr");
