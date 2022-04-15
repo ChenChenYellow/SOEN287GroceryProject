19f437217d07bd33d91af3056453ce7cb3a10369
Yuchen Huang <YuchenHuang97@Gmail.com>
2022-04-14 23:23:07 -0400

    Update ShowAisle.js

--- Script/ShowAisle.js
+++ Script/ShowAisle.js
@@ -1,21 +1,32 @@
 import { xml2json } from "./xml2json.js";
 
 function readAislesXML(aisleName) {
-  return fetch("./Data/Inventory.xml") // request to access xml file
-    .then(function (response) { // the result of access is 'response'
-      return response.text(); // we pass response.text() to the next block
+  // Request to access xml file
+  // The result of access is 'response'
+  // We pass response.text() to the next block
+  return fetch("./Data/Inventory.xml") 
+    .then(function (response) { 
+      return response.text(); 
     })
-    .then(function (data) { // we receive response.text() and rename it 'data'
-      let parser = new DOMParser(); // we parse 'data' to a json object
+    // We receive response.text() and rename it 'data'
+    // We parse 'data' to a json object
+    // The json object is called 'ret'
+    // We pass the ret["inventory"] to next block
+    .then(function (data) { 
+      let parser = new DOMParser(); 
       let xmlDoc = parser.parseFromString(data, "text/xml");
-      let ret = xml2json(xmlDoc, "    "); // the json object is called 'ret'
-      return ret["inventory"]; // we pass the ret["inventory"] to next block
+      let ret = xml2json(xmlDoc, "    "); 
+      return ret["inventory"]; 
     })
-    .then(function (data) { // we receive ret["inventory"] and rename it 'data'
-      const ret = data["product"].filter((x) => { // data["product"] is an array of json, we loop through it
-        return x["aisle"] == aisleName; // and select all item whose "asile" match our aisle name
+    // We receive ret["inventory"] and rename it 'data'
+    // Data["product"] is an array of json, we loop through it
+    // And select all item whose "asile" match our aisle name
+    .then(function (data) { 
+      const ret = data["product"].filter((x) => { 
+        return x["aisle"] == aisleName; 
       });
-      return ret; // and return those selected items
+      // And return those selected items
+      return ret; 
     });
 }
 
@@ -67,7 +78,8 @@ window.addEventListener("load", async (event) => {
     cardBody.classList.add("card-body", "d-flex", "flex-column");
     let a = document.createElement("a");
     cardBody.appendChild(a);
-    a.href = "./product.html?id=" + products[i]["id"]; // When click a product, it goes to product.html with its product id in the url
+    // When click a product, it goes to product.html with its product id in the url
+    a.href = "./product.html?id=" + products[i]["id"]; 
     a.classList.add("text-decoration-none", "stretched-link");
     a.style.color = "inherit";
     let hr = document.createElement("hr");
