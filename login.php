9fe69d27e832e9b5e0757be6a8adc00728e82b08
Abdel <abdelkader.habel_pro@outlook.com>
2022-04-17 13:28:51 -0400

    added admin permission

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <form action="welcome.php" method="post">
+        <h1>Login</h1>
+        <h2>Enter your credentials to access the backstore functions of the grocery store</h2>
+        <label for="username">Username:</label>
+        <input type="text" name="username">
+        <label for="password">Password:</label>
+        <input type="password" name="password">
+        <input type="submit" name="login" value="Login">
+    </form>
+
+</body>
+
+</html>
