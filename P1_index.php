f461df5dfcf716a3c4d9e00766812e1d7c6cd359
Abdel <abdelkader.habel_pro@outlook.com>
2022-04-17 15:35:35 -0400

    changed extension p5-p6 from html to php

--- P1_index.php
+++ P1_index.php
@@ -1,139 +1,134 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="styles.css" />
-    <title>Freshco</title>
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
-    />
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-  </head>
 
-  <body>
-    <header>
-      <div class="wrapper">
-        <div id="logo">
-          <a href="index.html">FRESHCO</a>
-        </div>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="styles.css" />
+  <title>Freshco</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" />
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
 
-        <ul class="navbar-home">
-          <li><a href="index.html">HOME</a></li>
-          <li><a href="aisles.html">AISLES</a></li>
-          <li><a href="shoppingCart.html">CART</a></li>
-          <li><a href="P5.html">SIGN IN</a></li>
-          <li><a href="P6.html">SIGN UP</a></li>
-          <li><a href="ManageUsers.html">ADMIN</a></li>
-        </ul>
-        <div id="layover"></div>
-      </div>
-      <div id="hamburger-menu-home">
-        <span></span>
-        <span></span>
-        <span></span>
+<body>
+  <header>
+    <div class="wrapper">
+      <div id="logo">
+        <a href="index.html">FRESHCO</a>
       </div>
 
-      <div id="Welcome" class="container">
-        <h1 class="row">Fresher Than Anything, Tastier Than Everything</h1>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque
-          voluptas sequi voluptatem vitae. Alias porro facilis possimus
-          consequuntur distinctio repudiandae quia maxime soluta, eos, non, quae
-          nam laborum obcaecati voluptatum. Accusantium iusto exercitationem a,
-          eveniet ut distinctio excepturi perspiciatis eos hic recusandae
-          minima, voluptate itaque molestias voluptatibus pariatur, reiciendis
-          iste odio saepe praesentium sapiente! At doloribus suscipit totam
-          minima fugit?
-        </p>
-        <!-- <p>
+      <ul class="navbar-home">
+        <li><a href="index.html">HOME</a></li>
+        <li><a href="aisles.html">AISLES</a></li>
+        <li><a href="shoppingCart.html">CART</a></li>
+        <li><a href="P5.php">SIGN IN</a></li>
+        <li><a href="P6.php">SIGN UP</a></li>
+        <li><a href="ManageUsers.html">ADMIN</a></li>
+      </ul>
+      <div id="layover"></div>
+    </div>
+    <div id="hamburger-menu-home">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+
+    <div id="Welcome" class="container">
+      <h1 class="row">Fresher Than Anything, Tastier Than Everything</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque
+        voluptas sequi voluptatem vitae. Alias porro facilis possimus
+        consequuntur distinctio repudiandae quia maxime soluta, eos, non, quae
+        nam laborum obcaecati voluptatum. Accusantium iusto exercitationem a,
+        eveniet ut distinctio excepturi perspiciatis eos hic recusandae
+        minima, voluptate itaque molestias voluptatibus pariatur, reiciendis
+        iste odio saepe praesentium sapiente! At doloribus suscipit totam
+        minima fugit?
+      </p>
+      <!-- <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
           consectetur ducimus quis beatae tempore quae, totam dicta doloribus
           nulla facere reiciendis. Enim eum sequi quisquam architecto temporibus
           molestias ducimus delectus.
         </p> -->
-      </div>
-    </header>
-    <section id="content">
-      <br />
-      <div id="Covid">
-        <h2>COVID-19 Situation in our Stores</h2>
-        <p>
-          Before the food you buy from us reaches your plate, many steps are
-          taken behind the scenes to ensure only the cleanest and freshest
-          products are sold to you. At Freshco, we take your safety and that of
-          our employees very seriously and that is why we have many protocols to
-          make your shopping experience as safe as possible. We have partnered
-          with distributors and different providers to make sure warehouses and
-          delivery trucks are sanitized on a regular basis. Our stores follow
-          rigorous measures to avoid the spread of the virus with our employees,
-          by mandating social distancing in break areas or offices.
-        </p>
-        <p>
-          In front of the constantly evolving situation, our measures may be
-          adapted in respect to health and governmental authorities. Wearing a
-          face mask in store is mandatory. At the entrance of your store, an
-          employee will disinfect a cart for you to simplify your shopping. Hand
-          sanitizer is available at the entrance, next to every cash register
-          and throughout the store alleys. As much as possible, pay with a
-          contactless card, to avoid touching the PIN pad.
-        </p>
-        <p>
-          We thank you for your collaboration and wish you a truly enjoyable
-          shopping experience.
-        </p>
-        <img
-          src="https://www.intelligencecareers.gov/NSA/images/nsa/covid19-message.png"
-          alt="covid measures"
-        />
+    </div>
+  </header>
+  <section id="content">
+    <br />
+    <div id="Covid">
+      <h2>COVID-19 Situation in our Stores</h2>
+      <p>
+        Before the food you buy from us reaches your plate, many steps are
+        taken behind the scenes to ensure only the cleanest and freshest
+        products are sold to you. At Freshco, we take your safety and that of
+        our employees very seriously and that is why we have many protocols to
+        make your shopping experience as safe as possible. We have partnered
+        with distributors and different providers to make sure warehouses and
+        delivery trucks are sanitized on a regular basis. Our stores follow
+        rigorous measures to avoid the spread of the virus with our employees,
+        by mandating social distancing in break areas or offices.
+      </p>
+      <p>
+        In front of the constantly evolving situation, our measures may be
+        adapted in respect to health and governmental authorities. Wearing a
+        face mask in store is mandatory. At the entrance of your store, an
+        employee will disinfect a cart for you to simplify your shopping. Hand
+        sanitizer is available at the entrance, next to every cash register
+        and throughout the store alleys. As much as possible, pay with a
+        contactless card, to avoid touching the PIN pad.
+      </p>
+      <p>
+        We thank you for your collaboration and wish you a truly enjoyable
+        shopping experience.
+      </p>
+      <img src="https://www.intelligencecareers.gov/NSA/images/nsa/covid19-message.png" alt="covid measures" />
+    </div>
+
+    <div id="cards">
+      <div id="Hours">
+        <h2>Opening Hours</h2>
+        <ul>
+          <li>Sunday: 8:00 AM - 11:00 PM</li>
+          <li>Monday: 8:00 AM - 11:00 PM</li>
+          <li>Tuesday: 8:00 AM - 11:00 PM</li>
+          <li>Wednesday: 8:00 AM - 11:00 PM</li>
+          <li>Thursday: 8:00 AM - 11:00 PM</li>
+          <li>Friday: 8:00 AM - 11:00 PM</li>
+          <li>Saturday: 8:00 AM - 11:00 PM</li>
+        </ul>
       </div>
 
-      <div id="cards">
-        <div id="Hours">
-          <h2>Opening Hours</h2>
-          <ul>
-            <li>Sunday: 8:00 AM - 11:00 PM</li>
-            <li>Monday: 8:00 AM - 11:00 PM</li>
-            <li>Tuesday: 8:00 AM - 11:00 PM</li>
-            <li>Wednesday: 8:00 AM - 11:00 PM</li>
-            <li>Thursday: 8:00 AM - 11:00 PM</li>
-            <li>Friday: 8:00 AM - 11:00 PM</li>
-            <li>Saturday: 8:00 AM - 11:00 PM</li>
-          </ul>
-        </div>
-
-        <div id="address">
-          <h2>Locations</h2>
-          <ul>
-            <li>Montreal</li>
-            <p>1234 Boulevard Anyname</p>
-            <li>Laval</li>
-            <p>473 Avenue Freshco</p>
-            <li>Longueuil</li>
-            <p>3283 Rue Principale</p>
-          </ul>
-        </div>
-
-        <div id="students">
-          <h2>STUDENTS!!!</h2>
-          <p>
-            You are looking for a job? Need a flexible schedule? Are passionate
-            about helping people?
-          </p>
-          <br />
-          <p id="job-offer">
-            We have the solution for you!! Drop by any location and file an
-            employment form, you will be called for an interview, we guarantee
-            it!
-          </p>
-        </div>
+      <div id="address">
+        <h2>Locations</h2>
+        <ul>
+          <li>Montreal</li>
+          <p>1234 Boulevard Anyname</p>
+          <li>Laval</li>
+          <p>473 Avenue Freshco</p>
+          <li>Longueuil</li>
+          <p>3283 Rue Principale</p>
+        </ul>
       </div>
-    </section>
-    <!-- 
+
+      <div id="students">
+        <h2>STUDENTS!!!</h2>
+        <p>
+          You are looking for a job? Need a flexible schedule? Are passionate
+          about helping people?
+        </p>
+        <br />
+        <p id="job-offer">
+          We have the solution for you!! Drop by any location and file an
+          employment form, you will be called for an interview, we guarantee
+          it!
+        </p>
+      </div>
+    </div>
+  </section>
+  <!-- 
     <footer>
         <div id="socials">
             <h3>Follow us on Social Media</h3>
@@ -167,5 +162,6 @@
         </div>
 
     </footer> -->
-  </body>
+</body>
+
 </html>
